4852b19431420ec8bf6082cb338a14ee35e57052
Пользователь <cocosi383@gmail.com>
2023-04-06 00:04:25 +0300

    register

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path(
@@ -39,4 +38,5 @@ urlpatterns = [
         name='password_reset_complete_view',
     ),
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 ]
