b7a5e552fc401830fdd6f3483a81bc5a56effc60
Пользователь <cocosi383@gmail.com>
2023-04-14 21:28:17 +0300

    image detail

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class Image(models.Model):
@@ -29,8 +30,11 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
--- bookmarks/images/static/js/bookmarklet.js
+++ bookmarks/images/static/js/bookmarklet.js
@@ -8,7 +8,7 @@ var head = document.getElementsByTagName('head')[0];  // Get HTML head element
 var link = document.createElement('link'); // Create new link Element
 link.rel = 'stylesheet'; // set the attributes for link element
 link.type = 'text/css';
-link.href = styleUrl + '?r=' + Math.floor(Math.random()*9999999999999999);
+link.href = styleUrl + '?r=' + Math.floor(Math.random()*9999999999999999n);
 head.appendChild(link);  // Append link element to HTML head
 
 // load HTML
